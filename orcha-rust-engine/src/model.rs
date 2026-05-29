a7be5326e217e601c591b3ffb46acb254b7f9fdf
Your Name <ciel259@gmail.com>
2026-05-30 00:11:34 +0800

    add rust improvements for productions

--- orcha-rust-engine/src/model.rs
+++ orcha-rust-engine/src/model.rs
@@ -13,14 +13,20 @@ pub struct FieldInput {
 impl FieldInput {
 
     pub fn to_arrow(&self) -> Result<DataType, OrchaError> {
-
         match self.data_type.to_lowercase().as_str() {
             "integer" | "int" | "int32" => Ok(DataType::Int32),
             "bigint" | "int64" | "long" => Ok(DataType::Int64),
             "double" | "float64" => Ok(DataType::Float64),
             "float" | "float32" => Ok(DataType::Float32),
             "boolean" | "bool" => Ok(DataType::Boolean),
-            "varchar" | "string" | "text" => Ok(DataType::Utf8),
+            "varchar" | "string" | "text" | "char" | "uuid" => Ok(DataType::Utf8),
+            "date" => Ok(DataType::Date32),
+            "timestamp" | "datetime" | "timestamptz" => Ok(DataType::Timestamp(
+                datafusion::arrow::datatypes::TimeUnit::Millisecond,
+                None,
+            )),
+            "decimal" | "numeric" => Ok(DataType::Decimal128(38, 10)),
+            "binary" => Ok(DataType::Binary),
             _ => Err(OrchaError::Custom(format!(
                 "Unsupported database type: {}",
                 self.data_type
